5b16e6b08f295eeeae3b96cbfdab9897de15cef6
Amaury Chong <achong@carrasco.uruit.com>
2021-04-28 16:50:15 -0300

    fisrt changes

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log('Pressed');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Hello new React Native App!</Text>
+      <TouchableOpacity>
+        <Image
+        fadeDuration={2000}
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300"}}/>
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,6 +24,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginTop: 22,
     alignItems: 'center',
     justifyContent: 'center',
   },
