6006f33d661d707dbb316c52e2fb1dc8b5a1a2e4
Amaury Chong <achong@carrasco.uruit.com>
2021-04-29 16:20:03 -0300

    added some screens

--- App.js
+++ App.js
@@ -1,41 +1,13 @@
 import React from 'react';
 import { Dimensions, StyleSheet, SafeAreaView, StatusBar, Platform, View } from 'react-native';
 import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   const {landscape} = useDeviceOrientation();
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-      style={{
-        backgroundColor: '#fff',
-        flex: 1,
-        flexDirection: 'row'
-      }}
-      >
-        <View
-        style={{
-          backgroundColor: 'dodgerblue',
-          width: 100,
-          height: 100
-        }}
-        ></View>
-        <View
-        style={{
-          backgroundColor: 'gold',
-          width: 100,
-          height: 100
-        }}
-        ></View>
-        <View
-        style={{
-          backgroundColor: 'tomato',
-          width: 100,
-          height: 100
-        }}
-        ></View>
-      </View>
-    </SafeAreaView>
+    <ViewImageScreen />
   );
 }
 
